89ef93960a852c2617b0c0ff94e3d12316e9e44e
riccardo-giuri <richicryst@gmail.com>
2023-10-17 15:26:52 +0200

    cards component created and printed

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script>
   import axios from 'axios';
+  import Card from './components/Card.vue';
 
   export default {
+    components: {
+      Card
+    },
+
     data() {
       return {
         projects: []
@@ -13,8 +18,12 @@
         axios.get("http://127.0.0.1:8000/api/projects")
         .then((response) => {
           this.projects = response.data;
-          console.log(this.projects);
+          console.log(this.projects[0]);
         })
+      },
+
+      getImageURL(project) {
+        return `http://127.0.0.1:8000/storage/${project.imageURL}`
       }
     },
 
@@ -25,7 +34,17 @@
 </script>
 
 <template>
+  <h1 class="text-center pt-3">Lista dei progetti</h1>
+
+  <div class="container mt-4">
+      <div class="row">
+        <div class="col-4" v-for="project in this.projects" :key="project.id">
+          <Card :project="project" :image="getImageURL(project)"></Card>
+        </div>
+      </div>
+  </div>    
 </template>
 
 <style scoped>
+
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<script>
+    export default {
+        props: {
+            project: Object,
+            image: String
+        }
+    }
+</script>
+
+<template>
+
+    <div class="card">
+        <img :src="image" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Nome Progetto: {{project.title}}</h5>
+            <a href="#" class="btn btn-outline-dark">Dettagli progetto</a>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
