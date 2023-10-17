1d27bb0fd629048e54b09f721ee48a04c14ec13d
riccardo-giuri <richicryst@gmail.com>
2023-10-17 14:40:12 +0200

    basic file project setup with Api call tested

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+  import axios from 'axios';
+
+  export default {
+    data() {
+      return {
+        projects: []
+      }
+    },
+
+    methods: {
+      fetchData() {
+        axios.get("http://127.0.0.1:8000/api/projects")
+        .then((response) => {
+          this.projects = response.data;
+          console.log(this.projects);
+        })
+      }
+    },
+
+    mounted() {
+      this.fetchData();
+    }
+  }
+</script>
+
+<template>
+</template>
+
+<style scoped>
+</style>
