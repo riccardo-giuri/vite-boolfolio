0d04531b44d67370818586f9f1696ca862e556f7
riccardo-giuri <richicryst@gmail.com>
2023-10-18 17:30:06 +0200

    routes aggiunte con le chiamate alle API show e pages divise

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -13,7 +13,7 @@
         <img :src="image" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">Nome Progetto: {{project.title}}</h5>
-            <a href="#" class="btn btn-outline-dark">Dettagli progetto</a>
+            <RouterLink :to="{name:'projects', params: {project: project.slug}}" class="btn btn-outline-dark">Dettagli progetto</RouterLink>
         </div>
     </div>
 </template>
--- src/pages/ProjectShow.vue
+++ src/pages/ProjectShow.vue
@@ -1,12 +1,81 @@
 <script>
-    export default {
+  import axios from 'axios';
 
+  export default {
+    data() {
+      return {
+        project: {}
+      }
+    },
+
+    methods: {
+      fetchData() {
+        axios.get("http://127.0.0.1:8000/api/projects/" + this.$route.params.project)
+        .then((response) => {
+          this.project = response.data;
+          console.log(response.data);
+        })
+      },
+
+      getImageURL(project) {
+        return `http://127.0.0.1:8000/storage/${project.imageURL}`
+      }
+    },
+
+    mounted() {
+      this.fetchData();
     }
+  }
 </script>
 
 <template>
+    <h1 class="text-center pt-3">Dettagli del progetto</h1>
+
     <div class="container">
-        <h1>Benvenuti nella HOMEPAGE</h1>
+        <div class="row">
+            <div class="col-6">
+                <img src={{getImageURL()}} alt="" class="w-100">
+            </div>
+
+            <div class="col-6">
+                <ul class="comicDetails">
+                    <li>
+                        <span class="list_item_title">Titolo: </span>
+                        <span>{{project.title}}</span>
+                    </li>
+                    <li>
+                        <span class="list_item_title">Descrizione: </span>
+                        <span>{{project.description}}</span>
+                    </li>
+                    <li>
+                        <span class="list_item_title">Link progetto: </span>
+                        <span><a href="#">{{project.githubURL}}</a></span>  
+                    </li>
+                    <!-- <li>
+                        <span class="list_item_title">Tipologia di progetto: </span>
+                        <span>{{project.type.name}}</span>  
+                    </li> -->
+                    <!-- <li>
+                        <span class="list_item_title">Tipologie utilizzate: </span>
+                        @foreach ($project->tecnologies as $tecnology)
+                            <span class="badge" style="background-color: rgb({{$tecnology->color}})">{{$tecnology->name}}</span> 
+                        @endforeach 
+                    </li> -->
+                    <!-- <li>
+                        <div class="route_buttons d-flex gap-2 mt-2">
+                            <a href="{{route('admin.projects.edit', $project->slug)}}" class="btn btn-outline-dark">Modifica Progetto</a>
+                            <form action="{{route('admin.projects.destroy', $project->slug)}}" method="POST">
+                                @csrf
+
+                                @method('delete')
+
+                                <input type="submit" value="Cancella Progetto" class="btn btn-outline-dark">
+                            </form>
+                        </div>
+                    </li> -->
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
